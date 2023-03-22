904ec9b268d18fb1de17821e967b8d07162595d3
laurensgalmo <galvezmoyalaura@gmail.com>
2023-03-22 20:54:06 +0100

    Add nuevo ejercicio

---
+++ src/curso/javabasico/tema02/ejercicio01/ejercicio01.java
@@ -0,0 +1,50 @@
+package curso.javabasico.tema02.ejercicio01;
+
+/**
+ * Para este primer ejercicio tendréis que realizar lo siguiente:
+ * 
+ * Crea un proyecto de Java desde 0
+ * 
+ * Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que
+ * crear una clase.
+ * 
+ * Dentro de la clase tenéis que crear el método main e imprimir todos los datos
+ * que se han visto en las sesiones.
+ * 
+ * Recordatorio: Los tipos de datos más comunes son int, long, double, boolean,
+ * String.
+ * 
+ * @author Laura
+ *
+ */
+
+public class ejercicio01 {
+
+	public static void main(String[] args) {
+
+		// Enteros
+
+		byte variable = 5;
+		short variable2 = 10;
+		int variable3 = 30;
+		long variable4 = 100;
+		long variableNumTelefono = 655655655;
+
+		// Decimales
+
+		float variable5 = 5.5f;
+		double variable6 = 10.5d;
+		double variablePrecioSilla = 49.99;
+		variable6 = 20.5f;
+
+		// Booleanos
+
+		boolean variable7 = false;
+		boolean variable8 = true;
+
+		// Texto
+
+		char variable9 = 'a';
+		String variable10 = "Open Bootcamp";
+	}
+}
